f3d2a25c21e9148e0705abf9e2f2359eff82f7d5
Vudang <danglegiavu@gmail.com>
2021-10-17 19:02:18 +0700

    implement singleton pattern

---
+++ Singleton/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Singleton
+{
+    public sealed class Singleton
+    {
+        public static Singleton Instance { get; } = new Singleton();
+        private Singleton()
+        {
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton instance_one = Singleton.Instance;
+            Singleton instance_two = Singleton.Instance;
+            Console.WriteLine(instance_one.Equals(instance_two));
+        }
+    }
+}
